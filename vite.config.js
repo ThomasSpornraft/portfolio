94703f0467c86c91d05c79ff60ae8cc74ddad981
Thomas Spornraft <thomas.spornraft@web.de>
2022-07-04 13:16:35 +0200

    vite.config korrigiert, deploy.sh++

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
+const { resolve } = require('path')
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [vue()]
-
+  plugins: [vue()],
 })
