892c87b0f8838187eaf731742ccb1ad8d0f46abd
Thomas Spornraft <thomas.spornraft@web.de>
2022-07-04 10:28:05 +0200

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://thomasspornraft.github.io/portfolio/',
+  base: '/portfolio/',
   plugins: [vue()]
+
 })
